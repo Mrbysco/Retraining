9c6bc87745833f6c58b76dd018a10157f209f58b
Mrbysco <Mrbysco@users.noreply.github.com>
2023-11-27 03:34:33 +0100

    Use the abstracted method

--- forge/src/main/java/com/mrbysco/retraining/network/messages/ResetTradesMessage.java
+++ forge/src/main/java/com/mrbysco/retraining/network/messages/ResetTradesMessage.java
@@ -1,15 +1,8 @@
 package com.mrbysco.retraining.network.messages;
 
-import com.mrbysco.retraining.mixin.AbstractVillagerEntityAccessor;
-import com.mrbysco.retraining.mixin.MerchantMenuAccessor;
-import com.mrbysco.retraining.mixin.VillagerAccessor;
+import com.mrbysco.retraining.CommonRetraining;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.npc.Villager;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.MerchantMenu;
-import net.minecraft.world.item.trading.Merchant;
-import net.minecraft.world.item.trading.MerchantOffers;
 import net.minecraftforge.network.NetworkEvent.Context;
 
 import java.util.function.Supplier;
@@ -31,20 +24,7 @@ public class ResetTradesMessage {
 		ctx.enqueueWork(() -> {
 			if (ctx.getDirection().getReceptionSide().isServer() && ctx.getSender() != null) {
 				ServerPlayer player = ctx.getSender();
-				AbstractContainerMenu container = player.containerMenu;
-				if (container instanceof MerchantMenu) {
-					Merchant merchant = ((MerchantMenuAccessor) container).getTrader();
-					if (merchant instanceof Villager villager) {
-						if (villager.getVillagerXp() == 0) {
-							MerchantOffers newOffers = new MerchantOffers();
-							((AbstractVillagerEntityAccessor) villager).setOffers(newOffers);
-							((VillagerAccessor) villager).invokeUpdateTrades();
-							((VillagerAccessor) villager).invokeUpdateSpecialPrices(player);
-							player.sendMerchantOffers(container.containerId, newOffers, villager.getVillagerData().getLevel(),
-									villager.getVillagerXp(), villager.showProgressBar(), villager.canRestock());
-						}
-					}
-				}
+				CommonRetraining.resetTrades(player);
 			}
 		});
 		ctx.setPacketHandled(true);
