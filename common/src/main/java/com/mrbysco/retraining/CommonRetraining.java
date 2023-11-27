c9eb7122d85090aa556017690f984b1dcddfbd48
Mrbysco <Mrbysco@users.noreply.github.com>
2023-11-27 03:34:20 +0100

    Abstract out the trade resetting and don't reset if an item is in the 3rd slot

--- common/src/main/java/com/mrbysco/retraining/CommonRetraining.java
+++ common/src/main/java/com/mrbysco/retraining/CommonRetraining.java
@@ -1,9 +1,36 @@
 package com.mrbysco.retraining;
 
+import com.mrbysco.retraining.mixin.AbstractVillagerEntityAccessor;
+import com.mrbysco.retraining.mixin.MerchantMenuAccessor;
+import com.mrbysco.retraining.mixin.VillagerAccessor;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MerchantMenu;
+import net.minecraft.world.item.trading.Merchant;
+import net.minecraft.world.item.trading.MerchantOffers;
+
 public class CommonRetraining {
 	public static boolean isVillager;
 
 	public static void init() {
 		isVillager = false;
+	}
+
+	public static void resetTrades(ServerPlayer player) {
+		AbstractContainerMenu container = player.containerMenu;
+		if (container instanceof MerchantMenu merchantMenu && !merchantMenu.getSlot(2).hasItem()) {
+			Merchant merchant = ((MerchantMenuAccessor) container).getTrader();
+			if (merchant instanceof Villager villager) {
+				if (villager.getVillagerXp() == 0) {
+					MerchantOffers newOffers = new MerchantOffers();
+					((AbstractVillagerEntityAccessor) villager).setOffers(newOffers);
+					((VillagerAccessor) villager).invokeUpdateTrades();
+					((VillagerAccessor) villager).invokeUpdateSpecialPrices(player);
+					player.sendMerchantOffers(container.containerId, newOffers, villager.getVillagerData().getLevel(),
+							villager.getVillagerXp(), villager.showProgressBar(), villager.canRestock());
+				}
+			}
+		}
 	}
 }
