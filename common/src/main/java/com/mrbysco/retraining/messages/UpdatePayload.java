1bac0e70254bda99da3ac2d446b2f69bc5526692
Mrbysco <Mrbysco@users.noreply.github.com>
2025-02-02 23:35:00 +0100

    Change update payload streamcodec to a composite streamcodec

--- common/src/main/java/com/mrbysco/retraining/messages/UpdatePayload.java
+++ common/src/main/java/com/mrbysco/retraining/messages/UpdatePayload.java
@@ -2,25 +2,21 @@ package com.mrbysco.retraining.messages;
 
 import com.mrbysco.retraining.Constants;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
 public record UpdatePayload(boolean villager, int experience) implements CustomPacketPayload {
-	public static final StreamCodec<FriendlyByteBuf, UpdatePayload> CODEC = CustomPacketPayload.codec(
-			UpdatePayload::write,
-			UpdatePayload::new);
+	public static final StreamCodec<FriendlyByteBuf, UpdatePayload> CODEC = StreamCodec.composite(
+			ByteBufCodecs.BOOL,
+			(payload) -> payload.villager,
+			ByteBufCodecs.INT,
+			(payload) -> payload.experience,
+			UpdatePayload::new
+	);
 	public static final Type<UpdatePayload> ID = new Type<>(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "update"));
-
-	public UpdatePayload(final FriendlyByteBuf packetBuffer) {
-		this(packetBuffer.readBoolean(), packetBuffer.readInt());
-	}
-
-	public void write(FriendlyByteBuf buf) {
-		buf.writeBoolean(villager);
-		buf.writeInt(experience);
-	}
 
 	@Override
 	@NotNull
